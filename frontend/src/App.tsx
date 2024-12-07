816a6515828049e4cd03b032ee34b578067f0ad9
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2024-12-06 23:08:30 -0600

    Updated CreateContenModal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,15 @@ import { ShareIcon } from './icons/ShareIcon'
 import { PlusIcon } from './icons/PlusIcon'
 import { Card } from './components/Card'
 import { CreateContentModal } from './components/CreateContentModal'
+import { useState } from 'react'
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return <div className='p-4'>
-    <CreateContentModal open={true} />
+    <CreateContentModal open={modalOpen} onClose={() => { setModalOpen(false) }} />
     <div className='flex justify-end gap-4'>
-      <Button variant="primary" text="Add Content" startIcon={<PlusIcon />}></Button>
+      <Button onClick={() => { setModalOpen(true) }} variant="primary" text="Add Content" startIcon={<PlusIcon />}></Button>
       <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon />}></Button>
     </div>
     <div className='flex gap-4'>
--- frontend/src/components/CreateContentModal.tsx
+++ frontend/src/components/CreateContentModal.tsx
@@ -10,12 +10,18 @@ export function CreateContentModal({ open, onClose }) {
         {open && <div className="w-screen h-screen bg-slate-500 fixed top-0 left-0 opacity-60 flex justify-center">
             <div className="flex flex-col justify-center">
                 <span className="bg-white opacity-100 p-4 rounded">
-                    <div className="flex justify-end"><XMark /></div>
+                    <div className="flex justify-end">
+                        <div onClick={onClose} className="cursor-pointer hover:bg-red-600 hover:text-white rounded p-2">
+                            <XMark />
+                        </div>
+                    </div>
                     <div>
                         <Input placeholder={"tiltle"} />
                         <Input placeholder={"link"} />
                     </div>
-                    <Button variant="primary" text="Submit"></Button>
+                    <div className="flex justify-center">
+                        <Button variant="primary" text="Submit"></Button>
+                    </div>
                 </span>
             </div>
         </div>}
