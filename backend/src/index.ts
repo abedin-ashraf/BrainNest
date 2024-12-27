71b39d334620ab29f56d8922f8a9cfdd903d4c96
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2024-12-27 12:50:02 -0600

    Updated backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -128,10 +128,10 @@ app.get("/api/v1/content", userMiddleWare, async (req, res) => {
 })
 app.delete("/api/v1/content", userMiddleWare, async (req, res) => {
     const contentId = req.body.contentId;
-    await ContentModel.deleteOne({
+    await ContentModel.deleteMany({
         contentId,
         //@ts-ignore
-        // userId: req.userId
+        userId: req.userId
     })
     res.json({
         message: "Deleted"
